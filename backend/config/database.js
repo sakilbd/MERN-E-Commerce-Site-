fa0571bf5f48bf914f4aee2eb551385bfafb4e49
Sakil <mdsakiluzzaman@gmail.com>
2022-05-31 17:09:37 +0600

    mongoDb connection working after mongodb installation

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URL, {
-        useNewUrlParse: true,
+
         useUnifiedTopology: true,
-        useCreateIndex: true
+
     }).then(con => {
         console.log(`MongoDb Datatbase connected with HOST: ${con.connection.host}`)
     })
