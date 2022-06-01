db464ef7bd2e1dd70f7335e1af2e6e59c1b172c2
Sakil <mdsakiluzzaman@gmail.com>
2022-06-01 12:26:27 +0600

    product delete api added

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,16 +1,20 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
+const {
+    getProducts,
+    newProduct,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+} = require("../controllers/productController");
 
+router.route("/products").get(getProducts);
+router.route("/product/:id").get(getSingleProduct);
 
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
+router.route("/admin/products/new").post(newProduct);
 
-router.route('/products').get(getProducts);
-router.route('/product/:id').get(getSingleProduct);
+router.route("/admin/product/:id").put(updateProduct)
+    .delete(deleteProduct)
 
-router.route('/admin/products/new').post(newProduct);
-
-router.route('/admin/product/:id').put(updateProduct);
-
-
-module.exports = router
+module.exports = router;
