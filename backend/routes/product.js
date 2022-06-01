ae0b54921647f244ba6f170bb04b84fd3465f95c
Sakil <mdsakiluzzaman@gmail.com>
2022-06-01 12:12:22 +0600

    update product done

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 
 
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/products/new').post(newProduct);
+router.route('/admin/products/new').post(newProduct);
+
+router.route('/admin/product/:id').put(updateProduct);
+
 
 module.exports = router
